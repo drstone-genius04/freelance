4b3ea2003a19c1abc0d75ab8a784f724c6dbff7e
Rishi More <rishimore102@gmail.com>
2024-02-16 22:42:59 +0530

    Added Files

--- myposh/posh/urls.py
+++ myposh/posh/urls.py
@@ -2,9 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.indexPage.as_view(), name='index'),
-    path('about/', views.aboutUs, name='about'),
-    path('contact/', views.contactUs, name='contact'),
-    path('services/', views.services, name='services'),
-   
+    path('', views.indexPage.as_view(), name='index')
+    
 ]
