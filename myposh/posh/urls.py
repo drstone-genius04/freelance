6e1bbdf9de071355996898dc3f17e6d8347a52fd
allan <kingallan.10@gmail.com>
2024-02-18 18:09:52 +0530

    images added

--- myposh/posh/urls.py
+++ myposh/posh/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.indexPage.as_view(), name='index')
+    path('', views.indexPage.as_view(), name='index'),
+    path('about/',views.aboutUs),
+    path('contact/',views.contact),
+    path('service/',views.service)
     
 ]
--- myposh/posh/views.py
+++ myposh/posh/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 from django.views import View
 
+
 # Create your views here.
 
 class indexPage(View):
     template_name = 'index.html'
     def get(self, request):
         return render(request, self.template_name)
-    
+
+def aboutUs(request):
+    return render(request,"about.html")
+def contact(request):
+    return render(request,"contact.html")
+def service(request):
+    return render(request,"services.html")
     # def get_context_data(self, **kwargs):
     #     context = super().get_context_data(**kwargs)
     #     context["main"] = context['main'].filter(user=self.request.user) 
